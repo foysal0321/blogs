bdf5727386c26c3ae9f15f01c47e57696d4e7e8c
Foysal Hossain <foysalx4@gmail.com>
2024-04-24 20:47:51 +0600

    blogs-fetch

--- src/components/home/blogs/Blogs.tsx
+++ src/components/home/blogs/Blogs.tsx
@@ -1,57 +1,80 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Image from "next/image";
 import LatestPost from "./LatestPost";
 import Container from "@/components/shared/Container";
 
-export default function Blogs() {
+const Blogs = () => {
+
+    const [posts, setPosts]: any[] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
+    const baseUrl = 'https://blog.jrrecyclingsolutionsltd.com.bd'
+
+    console.log(posts);
+
+    // Function to get the URL of the featured image
+    const fetchImage = async (mediaId: any) => {
+        if (!mediaId) return '/path/to/default/image.png'; // Provide a default image path
+        try {
+            const result = await axios.get(`${baseUrl}/wp-json/wp/v2/media/${mediaId}`);
+            return result.data.source_url;
+        } catch (error) {
+            console.error('Error fetching image:', error);
+            return '/path/to/default/image.png'; // Fallback image path
+        }
+    };
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`${baseUrl}/wp-json/wp/v2/posts`);
+
+                const postData = await Promise.all(response.data.map(async (post: any) => {
+                    const imageUrl = await fetchImage(post.featured_media);
+                    return {
+                        ...post,
+                        imageUrl
+                    };
+                }));
+                setPosts(postData);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Failed to fetch posts:', error);
+                setError("Failed to load posts.");
+                setIsLoading(false);
+            }
+        };
+
+        fetchPosts();
+    }, [baseUrl]);
+
+
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
     return (
         <Container>
             <div className="flex flex-wrap p-5 gap-6">
-                <div className="max-w-[760px] mx-auto">
-                    <div className=" grid md:grid-cols-2 grid-cols-1 gap-4">
-                        <div className="max-w-[370px]">
-                            <Image className="md:w-[370px] w-[350px]" src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1462798209360-48743134a7c9-1024x682.jpg'
-                                alt="blog" width={370} height={300}
-                            />
-                            <div className="text-center py-4 ">
-                                <p className="text-xs text-gray-600"> <span className="text-[#38B5F4] font-semibold">EUROPE</span>  - MAY 15, 2016</p>
-                                <h2 className="text-xl py-4 font-semibold">10 Ideas To Spice up Your Next Hiking Trip</h2>
-                                <p className=" text-gray-700">More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous negative one inside gorilla and drew humbly shot…</p>
-                                <button className="btn-outline btn-sm text-[#38B5F4] underline mt-6 font-semibold ">Read More</button>
-                            </div>
-                        </div>
-                        <div className="max-w-[370px]">
-                            <Image className="md:w-[370px] w-[350px]" src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1462798209360-48743134a7c9-1024x682.jpg'
-                                alt="blog" width={370} height={300}
-                            />
-                            <div className="text-center py-4">
-                                <p className="text-xs text-gray-600"> <span className="text-[#38B5F4] font-semibold">EUROPE</span>  - MAY 15, 2016</p>
-                                <h2 className="text-xl py-4 font-semibold">10 Ideas To Spice up Your Next Hiking Trip</h2>
-                                <p className=" text-gray-700">More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous negative one inside gorilla and drew humbly shot…</p>
-                                <button className="btn-outline btn-sm text-[#38B5F4] underline mt-6 font-semibold">Read More</button>
-                            </div>
-                        </div>
-                        <div className="max-w-[370px]">
-                            <Image className="md:w-[370px] w-[350px]" src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1462798209360-48743134a7c9-1024x682.jpg'
-                                alt="blog" width={370} height={300}
-                            />
-                            <div className="text-center py-4">
-                                <p className="text-xs text-gray-600"> <span className="text-[#38B5F4] font-semibold">EUROPE</span>  - MAY 15, 2016</p>
-                                <h2 className="text-xl py-4 font-semibold">10 Ideas To Spice up Your Next Hiking Trip</h2>
-                                <p className=" text-gray-700">More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous negative one inside gorilla and drew humbly shot…</p>
-                                <button className="btn-outline btn-sm text-[#38B5F4] underline mt-6 font-semibold">Read More</button>
-                            </div>
-                        </div>
-                        <div className="max-w-[370px]">
-                            <Image className="md:w-[370px] w-[350px]" src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1462798209360-48743134a7c9-1024x682.jpg'
-                                alt="blog" width={370} height={300}
-                            />
-                            <div className="text-center py-4">
-                                <p className="text-xs text-gray-600"> <span className="text-[#38B5F4] font-semibold">EUROPE</span>  - MAY 15, 2016</p>
-                                <h2 className="text-xl py-4 font-semibold">10 Ideas To Spice up Your Next Hiking Trip</h2>
-                                <p className=" text-gray-700">More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous negative one inside gorilla and drew humbly shot…</p>
-                                <button className="btn-outline btn-sm text-[#38B5F4] underline mt-6 font-semibold">Read More</button>
-                            </div>
-                        </div>
+                <div>
+                    <div className="max-w-[760px] mx-auto grid md:grid-cols-2 grid-cols-1 gap-4">
+                        {
+                            posts.map((post: any) => (
+                                <div className="max-w-[370px]" key={post.id}>
+                                    <Image className="md:w-[370px] w-[350px]" src={post.imageUrl}
+                                        alt={post.title.rendered} width={370} height={300}
+                                    />
+                                    <div className="text-center py-4 ">
+                                        <p className="text-xs text-gray-600"> {post.date}</p>
+                                        <h2 className="text-xl py-4 font-semibold">{post.title.rendered}</h2>
+                                        <p className=" text-gray-700">More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous negative one inside gorilla and drew humbly shot…</p>
+                                        <button className="btn-outline btn-sm text-[#38B5F4] underline mt-6 font-semibold ">Read More</button>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                     <div className="join w-full py-4">
                         <input className="join-item btn btn-square" type="radio" name="options" aria-label="1" checked />
@@ -61,11 +84,14 @@ export default function Blogs() {
                         <input className="join-item btn btn-square" type="radio" name="options" aria-label="5" />
                     </div> <hr />
                 </div>
-                <div className="max-w-[400px] mx-auto">
+                <div className="md:w-[400px] w-[350px] mx-auto">
                     <LatestPost />
                 </div>
-
             </div>
         </Container>
-    )
-}
+    );
+};
+
+export default Blogs;
+
+
--- src/components/home/blogs/LatestPost.tsx
+++ src/components/home/blogs/LatestPost.tsx
@@ -5,28 +5,28 @@ export default function LatestPost() {
     return (
         <div >
             <h1 className="text-xl text-[#38B5F4] font-semibold text-center md:mt-1 mt-4">LATEST POSTS</h1>
-            <div className="max-w-[400px]  flex flex-wrap  items-center gap-6 mt-4 ">
+            <div className="md:w-[400px] w-[330px] flex flex-wrap  items-center gap-6 mt-4 ">
                 <Image src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1474447092129-94d386577849-1170x780.jpg' width={160} height={200} alt='blog' />
                 <div className="md:w-[200px]">
                     <h4 className="text-zinc-900 font-medium">Epic Sea View of Santorini, Greece</h4>
                     <p className='text-gray-600 py-2 text-xs '>MAY 15, 2016</p>
                 </div>
             </div>
-            <div className="max-w-[400px]  flex flex-wrap  items-center gap-6 mt-4 ">
+            <div className="md:w-[400px] w-[330px] flex flex-wrap  items-center gap-6 mt-4 ">
                 <Image src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1474447092129-94d386577849-1170x780.jpg' width={160} height={200} alt='blog' />
                 <div className="md:w-[200px]">
                     <h4 className="text-zinc-900 font-medium">10 Ideas To Spice up Your Next Hiking Trip</h4>
                     <p className='text-gray-600 py-2 text-xs '>MAY 15, 2016</p>
                 </div>
             </div>
-            <div className="max-w-[400px]  flex flex-wrap  items-center gap-6 mt-4 ">
+            <div className="md:w-[400px] w-[330px] flex flex-wrap  items-center gap-6 mt-4 ">
                 <Image src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1474447092129-94d386577849-1170x780.jpg' width={160} height={200} alt='blog' />
                 <div className="md:w-[200px]">
                     <h4 className="text-zinc-900 font-medium">10 Ideas To Spice up Your Next Hiking Trip</h4>
                     <p className='text-gray-600 py-2 text-xs '>MAY 15, 2016</p>
                 </div>
             </div>
-            <div className="md:w-[400px] w-[350px] h-[120px] flex flex-wrap  items-center gap-6 mt-4 ">
+            <div className="md:w-[400px] w-[330px] h-[120px] flex flex-wrap  items-center gap-6 mt-4 ">
                 <Image src='https://cheerup2.theme-sphere.com/travel/wp-content/uploads/sites/6/2016/05/photo-1474447092129-94d386577849-1170x780.jpg' width={160} height={200} alt='blog' />
                 <div className="md:w-[200px]">
                     <h4 className="text-zinc-900 font-medium">A Calm Comes Over The Lake This Month</h4>
@@ -34,7 +34,7 @@ export default function LatestPost() {
                 </div>
             </div>
 
-            <div className="mt-8 p-4 max-w-[400px] ">
+            <div className="mt-8 p-4 md:w-[400px] w-[330px]">
                 <h1 className="text-xl text-[#38B5F4] font-semibold text-center py-4  md:mt-2 mt-5">CATEGORIES  </h1>
                 <div className="flex flex-wrap justify-between py-4">
                     <button>America</button>
